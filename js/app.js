688b050833b77b2a0508fb92d83ab6274fe21d81
baiyang <459676797@qq.com>
2016-07-06 14:22:38 +0800

    你好啊

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 //api_uri = "http://123.206.84.74/api/";
 api_uri = "http://api.supeiyunjing.com/";
 //api_uri = "http://172.17.2.13:8080/api/";
-templates_root = "/templates/";
+templates_root = "/app/zhironghao/templates/";
 deskey = "abc123.*abc123.*abc123.*abc123.*";
 
 var myApp = angular.module('myApp', [
