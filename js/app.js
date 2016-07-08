9a2c179a74b1a895d74913370f5d4da20d3011e6
itjiangzhuang <itjiangzhuang@163.com>
2016-07-08 10:22:58 +0800

    微信分享

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ api_uri = "http://123.206.84.74/api/";
 //api_uri = "http://172.17.2.13:8080/api/";
 templates_root = "/templates/";
 deskey = "abc123.*abc123.*abc123.*abc123.*";
-share_url = "http://app.zhironghao.com";
+share_url = "http://test.zhironghao.com";
 
 var myApp = angular.module('myApp', [
     'ng', 'ngRoute', 'ngAnimate', 'loginCtrl', 'registerCtrl', 'articleCtrl','userCtrl','ngTouchstart','ngTouchmove','ngTouchend'
