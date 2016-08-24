aa5253a43da85d2343761b1427fcc872018c4646
zrh <459676797@qq.com>
2016-08-24 17:59:38 +0800

    dasf

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ var myApp = angular.module('myApp', [
                     if (d.returnCode == 0) {
                         $rootScope.shareBankname = d.result.bankname + '-';
                         $rootScope.shareProductName = d.result.name;
-                        $rootScope.desc = '额度：'+d.result.loanvalue +'期限：'+d.result.loanlife+'利息：'+d.result.rate;
+                        $rootScope.desc = '额度：'+d.result.loanvalue +'/n期限：'+d.result.loanlife+'/n利息：'+d.result.rate;
                     } else {
                         console.log(d);
                     }
